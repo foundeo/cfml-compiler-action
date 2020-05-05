74fa3cbb54ac8c1fb6c2195535b2898d7bf553d4
Pete Freitag <pete@foundeo.com>
2020-05-05 12:22:18 -0400

    Remove output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-result=$(box cfcompile sourcePath=/github/workspace/ destPath=/tmp/)
-echo "::set-output name=output::$result"
+box cfcompile sourcePath=/github/workspace/ destPath=/tmp/ cfengine=$1
